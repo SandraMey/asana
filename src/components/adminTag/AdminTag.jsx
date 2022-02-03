26e7e5fc5e611931bcb8a2fa8dd25c1a01006f77
SandraMey <sandra.meyniel@gmail.com>
2022-02-03 14:38:46 +0100

    put road ok

--- src/components/adminTag/AdminTag.jsx
+++ src/components/adminTag/AdminTag.jsx
@@ -1,22 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './adminTag.scss';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./adminTag.scss";
 
 const AdminTag = () => {
   const [tags, setTags] = useState([]);
+  const [title, setTitle] = useState("");
 
   const getTag = async () => {
-    const { data } = await axios.get(
-      `${process.env.REACT_APP_API_URL}/tag`,
-    );
+    const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/tag`);
     setTags(data);
   };
   useEffect(() => {
     getTag();
   }, []);
 
-  const handleSubmitTag = async (id) => {
-    await axios.put(`${process.env.REACT_APP_API_URL}/tag/${id}`, { title: 1 });
+  const handleClickModify = async (e, id) => {
+    e.preventDefault();
+    const values = { title };
+    await axios.put(`${process.env.REACT_APP_API_URL}/tag/1`, values);
+    await getTag();
+  };
+  const handleClickModify2 = async (e, id) => {
+    e.preventDefault();
+    const values = { title };
+    await axios.put(`${process.env.REACT_APP_API_URL}/tag/2`, values);
+    await getTag();
+  };
+  const handleClickModify3 = async (e, id) => {
+    e.preventDefault();
+    const values = { title };
+    await axios.put(`${process.env.REACT_APP_API_URL}/tag/3`, values);
     await getTag();
   };
 
@@ -24,16 +37,53 @@ const AdminTag = () => {
     <div className="admintag">
       <h2>Liste des tag</h2>
       <div className="containertag">
-        {tags.map((d) => (
+        {tags.map((tag) => (
           <div className="tag">
-            <p className="pAdmin">{d.title}</p>
-            <input type="text" placeholder="Nouveau tag" id="title" />
-            <button type="button" onClick={() => handleSubmitTag(tags.id)}>Modifier</button>
+            <p className="pAdmin">{tag.title}</p>
           </div>
         ))}
+        <form>
+          <label htmlFor="title">Tag 1 : 
+            <input
+              id="title"
+              type="text"
+              placeholder="Nouveau tag"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </label>
+          <button type="submit" onClick={handleClickModify}>
+            Modifier
+          </button>
+        </form>
+        <form>
+          <label htmlFor="title">Tag 2 : 
+            <input
+              id="title"
+              type="text"
+              placeholder="Nouveau tag"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </label>
+          <button type="submit" onClick={handleClickModify2}>
+            Modifier
+          </button>
+        </form>
+        <form>
+          <label htmlFor="title">Tag 3 : 
+            <input
+              id="title"
+              type="text"
+              placeholder="Nouveau tag"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </label>
+          <button type="submit" onClick={handleClickModify3}>
+            Modifier
+          </button>
+        </form>
       </div>
     </div>
-  )
+  );
 };
 
 export default AdminTag;
